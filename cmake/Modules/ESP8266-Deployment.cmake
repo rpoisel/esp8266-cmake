d34e357a7623d15963831324fd3ff0e66f47f703
Rainer Poisel <rainer.poisel@gmail.com>
2015-12-11 21:30:41 +0000

    modular toolchain and SDK files

--- cmake/Modules/ESP8266-Deployment.cmake
+++ cmake/Modules/ESP8266-Deployment.cmake
@@ -4,13 +4,13 @@ function(create_deployment_rules binary_file_name)
         POST_BUILD
         COMMAND export PATH=$ENV{PATH}:${ESP8266_XTENSA_COMPILER_HOME}
         COMMAND ${ESP8266_ESPTOOL} elf2image -o ${CMAKE_BINARY_DIR}/ firmware${CMAKE_EXECUTABLE_SUFFIX}
-        BYPRODUCTS ${CMAKE_BINARY_DIR}/${FW_FILE_1} ${CMAKE_BINARY_DIR}/${FW_FILE_2}
+        BYPRODUCTS ${CMAKE_BINARY_DIR}/${FW_ADDR_1}.bin ${CMAKE_BINARY_DIR}/${FW_ADDR_2}.bin
     )
 
     add_custom_target(
         flash
         COMMAND export PATH=$ENV{PATH}:${ESP8266_XTENSA_COMPILER_HOME}
-        COMMAND ${ESP8266_ESPTOOL} --port ${ESP8266_ESPTOOL_COM_PORT} write_flash ${FW_ADDR_1} ${CMAKE_BINARY_DIR}/${FW_FILE_1} ${FW_ADDR_2} ${CMAKE_BINARY_DIR}/${FW_FILE_2}
-        DEPENDS ${CMAKE_BINARY_DIR}/${FW_FILE_1} ${CMAKE_BINARY_DIR}/${FW_FILE_2}
+        COMMAND ${ESP8266_ESPTOOL} --port ${ESP8266_ESPTOOL_COM_PORT} write_flash ${FW_ADDR_1} ${CMAKE_BINARY_DIR}/${FW_ADDR_1}.bin ${FW_ADDR_2} ${CMAKE_BINARY_DIR}/${FW_ADDR_2}.bin
+        DEPENDS ${CMAKE_BINARY_DIR}/${FW_ADDR_1}.bin ${CMAKE_BINARY_DIR}/${FW_ADDR_2}.bin
     )
 endfunction()
--- cmake/toolchain.ESP8266.512kB.cmake
+++
@@ -1,7 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/toolchain.ESP8266.cmake)
-
-set(ESP8266_LINKER_SCRIPT "${ESP8266_SDK_BASE}/ld/eagle.app.v6.ld")
-set(FW_FILE_1 0x00000.bin)
-set(FW_FILE_2 0x40000.bin)
-set(FW_ADDR_1 0x00000)
-set(FW_ADDR_2 0x40000)
